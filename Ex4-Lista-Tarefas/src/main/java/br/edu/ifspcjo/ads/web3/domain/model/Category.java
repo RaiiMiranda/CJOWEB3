16622e6d58683bc019e91e6e797f04aaf19b4d92
CJ3028941 <CJ3028941@192.168.56.1>
2025-09-15 22:30:05 -0300

    exercicio4

---
+++ Ex4-Lista-Tarefas/src/main/java/br/edu/ifspcjo/ads/web3/domain/model/Category.java
@@ -0,0 +1,68 @@
+package br.edu.ifspcjo.ads.web3.domain.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity // para o Hibernate/JPA saber que é uma tabela
+@Table(name = "categories") // para mapear para a tabela categories
+public class Category 
+{
+	
+	@Id // o campo id é identificado como chave primária
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // o banco vai gerar o campo id automaticamente (auto-increment)
+	private Long id;
+	
+	@NotNull
+	@Size(max = 50)
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	// hashCode() e equals() - garantem que objetos com mesmo id sejam tratados como mesmo usuário
+	
+	@Override
+	public int hashCode() 
+	{
+		// hashCode() -> Decide o balde (posição na tabela) que o id vai ficar
+		
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) 
+	{
+		// equals() -> Confirma se o objeto já existe naquele balde
+		
+		if (this == obj)
+			return true; 
+
+		if (obj == null)
+			return false;
+		
+		if (getClass() != obj.getClass()) 
+			return false;
+		
+		Category other = (Category) obj;
+		
+		return Objects.equals(id, other.id);
+	}
+	
+}
---
+++ Ex4-Lista-Tarefas/src/main/resources/db/migration/V01__criar_e_registrar_usuarios.sql
@@ -0,0 +1,15 @@
+CREATE TABLE user (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  name varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
+  password varchar(150) NOT NULL,
+  active tinyint(1) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO user (name, email, password, birth_date, gender, active) 
+	values ('Fernando Duarte', 'fernandoduarte@ifsp.edu.br', '$2a$10$Ot4XGuyPP7r82nN3WXA0bOL1Qk9gShKDlVuPoyp89HoFnHcwO4Tji', '1975-11-16', 'MASCULINO', 1);
+INSERT INTO user (name, email, password, birth_date, gender, active) 
+	values ('Juliana Silva', 'julianasilva@ifsp.edu.br', '$2a$10$Ot4XGuyPP7r82nN3WXA0bOL1Qk9gShKDlVuPoyp89HoFnHcwO4Tji', '1980-01-01', 'FEMININO', 1);
+INSERT INTO user (name, email, password, birth_date, gender, active) 
+	values ('Fernanda Donatella', 'fernandadonatella@ifsp.edu.br', '$2a$10$Ot4XGuyPP7r82nN3WXA0bOL1Qk9gShKDlVuPoyp89HoFnHcwO4Tji', '2000-05-05', 'FEMININO', 1);
