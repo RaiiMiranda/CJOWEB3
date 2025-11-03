94a9fb2c2ae4c3c0480e94c9c7b14b63e24eb02c
Raissa Miranda <raissa.miranda1801@gmail.com>
2025-11-03 10:36:34 -0300

    Delete ProjetoConstrutora directory

--- ProjetoConstrutora/src/main/java/br/edu/ifspcjo/ads/web3/construtora/service/exception/NonExistentOrInactiveUserException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifspcjo.ads.web3.construtora.service.exception;
-
-public class NonExistentOrInactiveUserException extends RuntimeException 
-{
-
-	private static final long serialVersionUID = 1L;
-
-}
